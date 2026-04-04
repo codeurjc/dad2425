cd8f9dc36a35de94e4001bb2dbb04093e9b45024
Francisco Gortázar <patxi.gortazar@gmail.com>
2026-04-04 18:21:53 +0200

    Update WebSecurityConfig.java with the new configuration

--- parte_5/ejem4b-haproxy-ssl-passthrough/src/main/java/es/codeurjc/daw/library/security/WebSecurityConfig.java
+++ parte_5/ejem4b-haproxy-ssl-passthrough/src/main/java/es/codeurjc/daw/library/security/WebSecurityConfig.java
@@ -5,14 +5,17 @@ import java.security.SecureRandom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	RepositoryUserDetailsService userDetailsService;
@@ -30,27 +33,30 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	
-    	// Public pages
-        http.authorizeRequests().antMatchers("/").permitAll();
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/loginerror").permitAll();
-        http.authorizeRequests().antMatchers("/logout").permitAll();
-
-        // Private pages
-        http.authorizeRequests().antMatchers("/newbook").hasAnyRole("USER");
-        http.authorizeRequests().antMatchers("/editbook/*").hasAnyRole("USER");
-        http.authorizeRequests().antMatchers("/removebook/*").hasAnyRole("ADMIN");
-
-        // Login form
-        http.formLogin().loginPage("/login");
-        http.formLogin().usernameParameter("username");
-        http.formLogin().passwordParameter("password");
-        http.formLogin().defaultSuccessUrl("/");
-        http.formLogin().failureUrl("/loginerror");
-
-        // Logout
-        http.logout().logoutUrl("/logout");
-        http.logout().logoutSuccessUrl("/");
+		
+		// Public pages
+        http.authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/loginerror").permitAll()
+            .antMatchers("/logout").permitAll()
+            // Private pages
+            .antMatchers("/newbook").hasAnyRole("USER")
+            .antMatchers("/editbook/*").hasAnyRole("USER")
+            .antMatchers("/removebook/*").hasAnyRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            // Login form
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .failureUrl("/loginerror")
+                .and()
+            // Logout
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
     }
 }
